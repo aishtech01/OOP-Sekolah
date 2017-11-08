0b1b2e01570433040a6107a5fa851fcad4c65e94
Aish <30832775+aishtech01@users.noreply.github.com>
2017-11-08 17:05:00 +0700

    Update Kelas.php

--- Kelas.php
+++ Kelas.php
@@ -10,21 +10,16 @@ class Kelas extends Mapel  {
 	public function tampilKelas() 
 	{
         return $this->kelas;
-<<<<<<< HEAD
-    }
+	}
 
-    public function idKelas($idkelas) 
+    	public function idKelas($idkelas) 
 	{
         $this->idkelas = $idkelas;
-    }
+    	}
 
 	public function tampilIdkelas() 
 	{
         return $this->idkelas;
-    }
+    	}
 
 }
-=======
-    	}
-}
->>>>>>> 368fee42e9c17d8a300d39e1ca8afcb69adf01de
