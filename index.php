efde075c20ad85819f29444a0d2b0f9b658a725f
Aish <30832775+aishtech01@users.noreply.github.com>
2017-11-08 08:41:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,4 +8,13 @@ echo "Nama Guru : " . $jadwal1->namaguru("Deni Sudiana, S.Pd.I")."<br>";
 echo "Kelas : " . $jadwal1->namakelas("X TKJ")."<br>";
 echo "========Daftar Siswa=======<br>";
 echo $jadwal1->namasiswa("Yunita")."<br>";
-echo $jadwal1->namasiswa("Rina")."<br>";
+echo $jadwal1->namasiswa("Rina")."<br><br>";
+
+$jadwal2 = new Jadwal();
+echo "Jadwal : ". $jadwal2->hari("Selasa")."<br>";
+echo "Mata Pelajaran : ". $jadwal2->namamapel("Matematika")."<br>";
+echo "Nama Guru : " . $jadwal2->namaguru("Asep Saepudin,S.Pd")."<br>";
+echo "Kelas : " . $jadwal2->namakelas("X TKJ")."<br>";
+echo "========Daftar Siswa=======<br>";
+echo $jadwal2->namasiswa("Yunita")."<br>";
+echo $jadwal2->namasiswa("Rina")."<br>";
