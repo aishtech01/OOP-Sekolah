8b6823c4a2399d1db8ad4d724cef9d9f2b3026ea
Aish <30832775+aishtech01@users.noreply.github.com>
2017-11-08 17:35:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,20 @@
 include 'Jadwal.php';
 
 $jadwal1 = new Jadwal();
+$siswa1 = new Jadwal();
+$siswa2 = new Jadwal();
+
+$siswa1->namaSiswa("Fujianti");
+$siswa2->namaSiswa("Riyani");
 
 $jadwal1->namaGuru("Yuni");
 $jadwal1->namaMapel("Bahasa Indonesia");
 $jadwal1->namaKelas("X TKJ");
-$jadwal1->namaSiswa("Riyani");
 $jadwal1->namaHari("Senin jam ke 2");
 
 echo "Waktu : ".$jadwal1->tampilHari()."<br>";
 echo "Mata Pelajaran : ".$jadwal1->tampilMapel()."<br>";
 echo "Nama Guru : ".$jadwal1->tampilNamaguru()."<br>";
 echo "Kelas : ".$jadwal1->tampilKelas()."<br>";
-echo "Nama Siswa : ".$jadwal1->tampilNamasiswa()."<br>";
+echo "Nama Siswa : ".$siswa1->tampilNamasiswa()."<br>";
+echo "Nama Siswa : ".$siswa2->tampilNamasiswa()."<br>";
