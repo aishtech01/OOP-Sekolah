e93d33a96be6d138a84afe0c9c7d85d5e1572a9d
Aish <30832775+aishtech01@users.noreply.github.com>
2017-11-08 19:12:26 +0700

    Update Mapel.php

--- Mapel.php
+++ Mapel.php
@@ -5,22 +5,22 @@ class Mapel extends Kelas {
 	public function namaMapel($mapel) 
 	{
         $this->mapel = $mapel;
-    }
+    	}
 
 	public function tampilMapel() 
 	{
         return $this->mapel;
-    }
+    	}
 
-    public function idMapel($idmapel) 
+    	public function idMapel($idmapel) 
 	{
         $this->idmapel = $idmapel;
-    }
+   	}
 
 	public function tampilIdmapel() 
 	{
         return $this->idmapel;
-    }
+    	}
 
 
 }
