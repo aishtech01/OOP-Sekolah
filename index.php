96b2eff36ab08c00b77deb42b798790714de7acb
Aish <30832775+aishtech01@users.noreply.github.com>
2017-11-09 09:50:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,20 +2,28 @@
 include 'Jadwal.php';
 
 $jadwal1 = new Jadwal();
-$siswa1 = new Jadwal();
-$siswa2 = new Jadwal();
+$siswa1 = new Siswa();
+$siswa2 = new Siswa();
+$guru1 = new Guru();
+$mapel1 = new Mapel();
+$kelas1 = new Kelas();
 
 $siswa1->namaSiswa("Fujianti");
 $siswa2->namaSiswa("Riyani");
 
-$jadwal1->namaGuru("Yuni");
-$jadwal1->namaMapel("Bahasa Indonesia");
-$jadwal1->namaKelas("X TKJ");
+$guru1->namaGuru("Yuni");
+
+$mapel1->namaMapel("Bahasa Indonesia");
+
+$kelas1->namaKelas("X TKJ");
+
 $jadwal1->namaHari("Senin jam ke 2");
 
+
+
 echo "Waktu : ".$jadwal1->tampilHari()."<br>";
-echo "Mata Pelajaran : ".$jadwal1->tampilMapel()."<br>";
-echo "Nama Guru : ".$jadwal1->tampilNamaguru()."<br>";
-echo "Kelas : ".$jadwal1->tampilKelas()."<br>";
+echo "Mata Pelajaran : ".$mapel1->tampilMapel()."<br>";
+echo "Nama Guru : ".$guru1->tampilNamaguru()."<br>";
+echo "Kelas : ".$kelas1->tampilKelas()."<br>";
 echo "Nama Siswa : ".$siswa1->tampilNamasiswa()."<br>";
 echo "Nama Siswa : ".$siswa2->tampilNamasiswa()."<br>";
